f8eff07ec55543ef68d3483bc3ea6f558d7e11f6
llimbri <leonlimbri@gmail.com>
2025-11-11 20:28:38 +1100

    Persist debug=False

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def toggle_navbar(mobile_opened, navbar):
     return navbar
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
