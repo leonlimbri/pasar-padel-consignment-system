1f2ff35f8918e2dc01a129663070e49bd5ef43cf
Leon Limbri <49016966+leonlimbri@users.noreply.github.com>
2025-12-31 00:09:37 +1100

    Login page finished

--- app.py
+++ app.py
@@ -1,140 +1,100 @@
+"""app.py
+Generate the Flask server and initialize the WebApp.
+Store the login-page layout + initialization of the layout-page
+"""
+
+import os, dotenv, json
 import dash_mantine_components as dmc
-import os
-from dash import Dash, html, Input, Output, State, callback, page_container, dcc, no_update
+from dash import Dash, html, dcc, Output, Input, State, no_update
 from flask import Flask, session
 
-# Server
-server=Flask(__name__)
-server.secret_key=os.environ.get("SECRET_KEY")
-app=Dash(
+# Flask Server + Initialize App
+# -----------------------------
+dotenv.load_dotenv()
+server = Flask(__name__)
+server.secret_key = os.environ.get("SECRET_KEY")
+USER_DETAILS = json.loads(os.environ.get("USER_DETAILS"))
+app = Dash(
     __name__,
-    server=server,
-    use_pages=True,
-    url_base_pathname="/",
+    server = server,
+    use_pages = True,
+    url_base_pathname = "/",
     suppress_callback_exceptions=True,
 )
+app.layout = dmc.MantineProvider(
+    children=[
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="protected-layout"),
+    ]
+)
 
-
-text_login={
-    "title": "Pasar Padel",
-    "subtitle-username": "Username", 
-    "placeholder-username": "Masukkan username anda", 
-    "subtitle-password": "Password", 
-    "placeholder-password": "Masukkan password anda", 
-    "button-login": "Login", 
-}
-
-# Login Layout
-login_layout=dmc.Center(
+# Login Page
+# ----------
+page_login = dmc.Center(
     dmc.Paper(
-        children=[
-            dmc.Title(
-                children=text_login.get("title"), 
-                order=2
-            ),
-            dmc.TextInput(
-                id="input-login", 
-                label=text_login.get("subtitle-username"), 
-                placeholder=text_login.get("placeholder-username"), 
-                mt="lg"
-            ),
-            dmc.PasswordInput(
-                id="input-password", 
-                label=text_login.get("subtitle-password"), 
-                placeholder=text_login.get("placeholder-password"), 
-                mt="sm"
-            ),
-            dmc.Button(
-                id="login-btn", 
-                children="Login", 
-                fullWidth=True, 
-                mt="sm", 
-                size="sm"
-            ),
-            dmc.Text(
-                id="login-message", 
-                size="xs", 
-                c="red"
-            ),
+         [
+            dmc.Title("Pasar Padel", order=2),
+            dmc.TextInput(label="Username", id="input-login", placeholder="Masukkan username anda", mt="lg"),
+            dmc.PasswordInput(label="Password", id="input-password", placeholder="Masukkan password anda", mt="sm"),
+            dmc.Button("Login", id="button-login", fullWidth=True, mt="sm", size="sm"),
+            dmc.Text(id="text-login-warning", size="xs", c="red")
         ],
         shadow="sm",
         p="xl",
         withBorder=True,
         style={"width": 350},
     ),
-    style={"height": "100vh"},
+    style={"height": "100vh"}
 )
 
-appshell_layout=dmc.AppShell(
-    header={"height": 75},
+# Page Layouts
+# ------------
+page_layout = dmc.AppShell(
+    [
+        dmc.Title("Test")
+    ],
+    header={"height": "5vh"},
     navbar={
         "width": 300,
         "breakpoint": "sm",
         "collapsed": {"mobile": True, "desktop": False},
     },
     padding="md",
-    id="appshell",
 )
 
-# # app.layout=dmc.MantineProvider(layout)
-# app.layout=dmc.MantineProvider(
-#     children=[
-#         dcc.Location(id="url", refresh=False),
-#         html.Div(id="protected-layout"),
-#     ]
-# )
+# CALLBACKS to handle any login / displaying protected layouts
+# ------------------------------------------------------------
+@app.callback(
+    Output("protected-layout", "children"),
+    Input("url", "pathname"),
+)
+def display_protected_pages(pathname):
+    if not session.get("logged_in") or pathname == "/logout":
+        session["logged_in"], session["user-id"], session["role"] = False, None, None
+        if pathname == "/login": return page_login
+        return dcc.Location(href="/login", id="redirect-login")
+    else:
+        return page_layout
 
-# @app.callback(
-#     Output("protected-layout", "children"),
-#     Input("url", "pathname"),
-#     prevent_initial_call=False,
-# )
-# def display_page(pathname):
-#     if not session.get("logged_in"):
-#         if pathname == "/login":
-#             return login_layout
-#         # redirect any unauthorized request to login
-#         return dcc.Location(href="/login", id="redirect-login")
+@app.callback(
+    Output("text-login-warning", "children"),
+    Output("url", "pathname"),
+    Input("button-login", "n_clicks"),
+    State("input-login", "value"),
+    State("input-password", "value"),
+)
+def handle_login(n_clicks, username, password):
+    if not n_clicks:
+        return no_update, no_update
 
-#     # handle logout
-#     if pathname == "/logout":
-#         session["logged_in"]=False
-#         session["user-id"]=None
-#         session["role"]=None
-#         return dcc.Location(href="/login", id="redirect-logout")
-
-#     # otherwise show full app
-#     return appshell_layout
-
-# @app.callback(
-#     Output("login-message", "children"),
-#     Output("url", "pathname", allow_duplicate=True),
-#     Input("login-btn", "n_clicks"),
-#     State("username", "value"),
-#     State("password", "value"),
-#     prevent_initial_call=True,
-# )
-# def handle_login(n_clicks, username, password):
-#     if not n_clicks:
-#         return no_update, no_update
-
-#     if username in USER_DETAILS and USER_DETAILS.get(username).get("password") == password:
-#         session["logged_in"]=True
-#         session["username"]=USER_DETAILS.get(username).get("name")
-#         session["role"]=USER_DETAILS.get(username).get("role")
-#         return "", "/"
-#     return "Invalid username or password", no_update
-
-# @callback(
-#     Output("appshell", "navbar"),
-#     Input("mobile-burger", "opened"),
-#     State("appshell", "navbar"),
-# )
-# def toggle_navbar(mobile_opened, navbar):
-#     navbar["collapsed"]={
-#         "mobile": not mobile_opened,
-#     }
-#     return navbar
+    print(username, password, USER_DETAILS)
+    if username in USER_DETAILS and USER_DETAILS.get(username).get("password") == password:
+        user = USER_DETAILS.get(username)
+        session["logged_in"] = True
+        for key in ["username", "role"]: session[key] = user.get(key)
+        return "", "/"
+    
+    return "Invalid username or password", no_update
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
