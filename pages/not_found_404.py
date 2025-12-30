7a173940303af903d1cc07ffad3f2be2062cc01e
Leon Limbri <49016966+leonlimbri@users.noreply.github.com>
2025-12-31 00:59:02 +1100

    Update 404 page

--- pages/not_found_404.py
+++ pages/not_found_404.py
@@ -1,11 +1,15 @@
 import dash_mantine_components as dmc
-from dash import register_page
+from dash import register_page, dcc
 
 register_page(__name__)
 layout=dmc.AppShellMain(
     [
-        dmc.Title("404 - Page Not Found"),
-        dmc.Text("The page you are looking for does not exist."),
-        dmc.NavLink("Go to Home Page", href="/"),
+        dmc.Title("404 - Halaman Tidak Ditemukan!"),
+        dmc.Text(
+            [
+                "Halaman yang anda cari tidak ada / belom terbuatkan. ",
+                dcc.Link("Kembali ke halaman utama", href="/"),
+            ],
+        )
     ]
 )
