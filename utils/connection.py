5de16de8c02bb237378a648d7b8f7edf3f3b17af
llimbri <leonlimbri@gmail.com>
2026-03-25 23:51:05 +1100

    Add exception try

--- utils/connection.py
+++ utils/connection.py
@@ -9,20 +9,23 @@ consignment_type_options = ["Racket", "Shirt", "Shoes", "Bag", "Others"]
 status_type_options = ["New", "Posted", "Sold", "Shipped", "Completed", "Completed Elsewhere"]
 
 def run_query(query: str):
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    if query.upper().startswith("INSERT") or query.upper().startswith("UPDATE") or query.upper().startswith("DELETE"):
-        cursor.execute(query)
-        conn.commit()
-        conn.close()
-    else:
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        dat = [dict(row) for row in rows]
-        conn.close()
-        return dat
-    return None
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        if query.upper().startswith("INSERT") or query.upper().startswith("UPDATE") or query.upper().startswith("DELETE"):
+            cursor.execute(query)
+            conn.commit()
+            conn.close()
+        else:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            dat = [dict(row) for row in rows]
+            conn.close()
+            return dat
+        return None
+    except Exception as e:
+        return None
 
 def run_query_from_sql(fn: str, fp: str = SQL_PATH, print_sql: bool=False,**kwargs):
     _fp = f"{fp}/{fn}"
